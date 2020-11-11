5e8f46022807f3ecda35339fbc4a582605189468
Johnatan Dos Santos Silva <Iroman250@gmail.com>
2020-11-10 21:21:36 -0300

    so vai soime conflito.

--- imoveis/src/app/app-routing.module.ts
+++ imoveis/src/app/app-routing.module.ts
@@ -2,31 +2,10 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  {
+ {
     path: '',
-    redirectTo: 'tabs',
-    pathMatch: 'full'
-  },
-  {
-    path: '',
-    loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
-  },
-  {
-    path: 'imovel/:id',
-    loadChildren: () => import('./home/property-details/property-details-routing.module').then( m => m.PropertyDetailsPageRoutingModule)
-  },
-  {
-    path: 'home',
     loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
-  },  {
-    path: 'home-list',
-    loadChildren: () => import('./home-list/home-list.module').then( m => m.HomeListPageModule)
-  },
-  {
-    path: 'property-details',
-    loadChildren: () => import('./property-details/property-details.module').then( m => m.PropertyDetailsPageModule)
   }
-
 
 ];
 @NgModule({
